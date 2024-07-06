f94baa41a48b9629a41a4acd954617ac9151addc
Nayubi <98013920+NayubiAR@users.noreply.github.com>
2024-07-06 22:41:13 +0800

    Membuat models untuk CRUD

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,32 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Idea;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index() {
+    public function index() 
+    {
+        $idea = new Idea([
+            'content' => 'Whatsupp',
+        ]);
+        $idea->save();
 
-            $users = [
-                [
-                    'name' => 'Alex',
-                    'age' => 30,
-                ],
-                [
-                    'name' => 'Dan',
-                    'age' => 69,
-                ],
-                [
-                    'name' => 'John',
-                    'age' => 17,
-                ]
-            ];
-
-            return view(
-                'dashboard',
-            [
-                'users' => $users
-            ]
-        );
+        return view('dashboard',[
+            'ideas' => Idea::orderBy('created_at','DESC')->get()
+        ]);
     }
 }
