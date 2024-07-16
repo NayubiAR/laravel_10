d9c3b01a591fb4ebea4f8d5018e4a085309d9ca9
Nayubi <98013920+NayubiAR@users.noreply.github.com>
2024-07-17 01:32:11 +0800

    Menambahkan show/menampilkan data menggunakan ID

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -7,30 +7,40 @@ use Illuminate\Http\Request;
 
 class IdeaController extends Controller
 {
-    public function store(){
 
-        request()->validate([
-            "idea"=> "required|min:5|max:240",
-        ],
-        [
-            'idea.required' => 'Ide wajib diisi.',
-            'idea.min' => 'Ide harus memiliki minimal :min huruf.',
-            'idea.max' => 'Ide tidak boleh lebih dari :max huruf.',
-        ]);
+    public function show(Idea $idea)
+    {
+        // Dengan menggunakan compact dapat mempersingkat code 'idea' => $idea yang mana bertujuan untuk mencari variable yang sama dari compact
+        // Dan akan membuatkan array asosiatif
+        return view('ideas.show', compact('idea'));
+    }
+    public function store()
+    {
+
+        request()->validate(
+            [
+                "idea" => "required|min:5|max:240",
+            ],
+            [
+                'idea.required' => 'Ide wajib diisi.',
+                'idea.min' => 'Ide harus memiliki minimal :min huruf.',
+                'idea.max' => 'Ide tidak boleh lebih dari :max huruf.',
+            ]
+        );
 
         $idea = Idea::create(
             [
-            'content' => request()->get('idea',''),
+                'content' => request()->get('idea', ''),
             ]
         );
 
         return redirect()->route('dashboard')->with('success', 'Idea Created Successfully!');
     }
 
-    public function destroy($id){
-        $idea = idea::where('id', $id)->firstOrFail();
+    public function destroy(Idea $id)
+    {
 
-        $idea->delete();
+        $id->delete();
 
         return redirect()->route('dashboard')->with('success', 'Idea deleted successfully!');
     }
