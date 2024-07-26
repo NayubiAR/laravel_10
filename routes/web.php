04bbe80c0a5dfc8a954cf4c17fd67e49db22c172
NayubiA1 <nayubiadiva@gmail.com>
2024-07-27 00:27:48 +0800

    Menambahkan upload image dan update image

--- routes/web.php
+++ routes/web.php
@@ -38,13 +38,15 @@ Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 //     });
 // });
 
-Route::resource('ideas', IdeaController::class)->except(['index','create', 'show'])->middleware('auth');
+Route::resource('ideas', IdeaController::class)->except(['index', 'create', 'show'])->middleware('auth');
 
 Route::resource('ideas', IdeaController::class)->only(['show']);
 
 Route::resource('ideas.comments', CommentController::class)->only(['store'])->middleware('auth');
 
-Route::resource('users', UserController::class)->only(['show','edit','update'])->middleware('auth');
+Route::resource('users', UserController::class)->only(['show', 'edit', 'update'])->middleware('auth');
+
+Route::get('profile', [UserController::class, 'profile'])->middleware('auth')->name('profile');
 
 // ------------------------------------------------- Resources Routing ----------------------------------------------------
 
